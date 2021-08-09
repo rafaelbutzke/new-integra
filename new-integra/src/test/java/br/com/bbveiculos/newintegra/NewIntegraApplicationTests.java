92980166960b63d8ee99db64261dad5e689b980d
Rafael Cristiano Butzke <butzke.rafa@gmail.com>
2021-08-08 23:20:54 -0300

    primeiro commit

---
+++ new-integra/src/test/java/br/com/bbveiculos/newintegra/NewIntegraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bbveiculos.newintegra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewIntegraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
